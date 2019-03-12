2d17e0ec5bf664257216f17b55fd8efd57d1eb76
Brooks Becton <broabect@ut.utm.edu>
2019-03-11 20:02:38 -0500

    Add Perf Checker

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import { Router } from "@reach/router";
 import * as React from "react";
 import { render } from "react-dom";
+import { registerObserver } from "react-perf-devtool";
 import { AppWrapper } from "./components/AppWrapper";
 import serviceWorker from "./registerServiceWorker";
 import { Calculate } from "./views/dashboard";
 import { PointDetail } from "./views/pointDetail/components/PointDetails";
+
+registerObserver()
+
 
 function App() {
   return (
---
+++ src/types/react-perf-devtool.d.ts
@@ -0,0 +1,3 @@
+declare module "react-perf-devtool" {
+  export function registerObserver(): void;
+}
