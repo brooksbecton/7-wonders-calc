36c8973b6fa37062bf00d5d44eb4c3bc87a2560e
Brooks Becton <bbecton@babelstreet.com>
2019-07-06 12:25:37 -0400

    Move Scroll Nav to AppWrapper

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -1,37 +1,61 @@
 import * as React from "react";
 import styled from "styled-components";
+
+import { useScroll } from "../hooks/useScroll";
 import pyramid from "./../icons/pyramid.svg";
 
+const TopBar = ({ children }: { children: any }) => {
+  const { y } = useScroll();
+  const TopBarNoScroll = styled.div`
+    justify-content: center;
+    background-color: #d1d1d1;
+    display: flex;
+    text-align: center;
+    width: 100%;
+
+    img {
+      padding: 10px;
+      height: 43px;
+      width: 43px;
+    }
+
+    p {
+      font-size: 34px;
+      margin: 0px;
+      padding: 0px;
+    }
+  `;
+
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "red",
+          display: y > 60 ? "block" : "none",
+          position: "fixed",
+          top: 0,
+          width: "100%",
+          zIndex: 1
+        }}
+      >
+        Top Bar
+      </div>
+      <TopBarNoScroll>{children}</TopBarNoScroll>
+    </>
+  );
+};
 export const AppWrapper: React.FunctionComponent = props => {
   return (
-    <Wrapper>
+    <>
       <TopBar>
         <img src={pyramid} alt="Yellow Pyramid Icon" />
       </TopBar>
-      <main>{props.children}</main>
-    </Wrapper>
+      <Wrapper>
+        <main>{props.children}</main>
+      </Wrapper>
+    </>
   );
 };
-
-const TopBar = styled.div`
-  justify-content: center;
-  background-color: #d1d1d1;
-  display: flex;
-  text-align: center;
-  width: 100%;
-
-  img {
-    padding: 10px;
-    height: 43px;
-    width: 43px;
-  }
-
-  p {
-    font-size: 34px;
-    margin: 0px;
-    padding: 0px;
-  }
-`;
 
 const Wrapper = styled.div`
   display: flex;
--- src/views/dashboard/components/Calculate.tsx
+++ src/views/dashboard/components/Calculate.tsx
@@ -2,27 +2,14 @@ import { RouteComponentProps } from "@reach/router";
 import * as React from "react";
 
 import { usePoints } from "./../../../hooks/usePoints";
-import { useScroll } from "./../../../hooks/useScroll";
 import { CalculateInput } from "./CalculateInput";
 import { TotalPoints } from "./TotalPoints";
 
 export const Calculate: React.FunctionComponent<RouteComponentProps> = () => {
   const [pointTypes, setPoint] = usePoints();
-  const { y } = useScroll();
-  console.log(y);
   return (
     <>
       <>
-        <div
-          style={{
-            backgroundColor: "red",
-            display: y > 60 ? "block" : "none",
-            position: "fixed",
-            top: 0
-          }}
-        >
-          Topbar
-        </div>
         {pointTypes.map(({ key, value, svg }) => {
           return (
             <CalculateInput
