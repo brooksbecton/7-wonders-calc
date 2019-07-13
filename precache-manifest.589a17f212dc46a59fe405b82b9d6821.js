22137ad5724482c7d6b6b6df28f322fbabcd409f
Brooks Becton <bbecton@babelstreet.com>
2019-07-13 10:07:07 -0400

    Updates

---
+++ precache-manifest.589a17f212dc46a59fe405b82b9d6821.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c8e08001fd5aa0a7f3cd66478319e6",
+    "url": "/7-wonders-calc/index.html"
+  },
+  {
+    "revision": "05e148ae770319dfd905",
+    "url": "/7-wonders-calc/static/js/2.dd2e1b9c.chunk.js"
+  },
+  {
+    "revision": "b4caf378bbafff133ad0",
+    "url": "/7-wonders-calc/static/js/main.8932e9a7.chunk.js"
+  },
+  {
+    "revision": "b485522e4bd8c830a84a",
+    "url": "/7-wonders-calc/static/js/runtime~main.248f7cb2.js"
+  },
+  {
+    "revision": "1a5c55a49e64ff7fca1fac496b609af1",
+    "url": "/7-wonders-calc/static/media/backArrow.1a5c55a4.svg"
+  },
+  {
+    "revision": "e7193c1f140ec58a66e91887dd90e3b5",
+    "url": "/7-wonders-calc/static/media/civilian.e7193c1f.svg"
+  },
+  {
+    "revision": "79cccdda69d806ce3a1336332e2cc012",
+    "url": "/7-wonders-calc/static/media/coin.79cccdda.svg"
+  },
+  {
+    "revision": "984e2a74ef03cf1cf92ddfa369dedb04",
+    "url": "/7-wonders-calc/static/media/commerce.984e2a74.svg"
+  },
+  {
+    "revision": "91f0e7279e53f0067df8d7729d364471",
+    "url": "/7-wonders-calc/static/media/guild.91f0e727.svg"
+  },
+  {
+    "revision": "0fce02a1756fbdc4350dd063657ef223",
+    "url": "/7-wonders-calc/static/media/military.0fce02a1.svg"
+  },
+  {
+    "revision": "2326794670ab0007d080a09dab1f9ca2",
+    "url": "/7-wonders-calc/static/media/pyramid.23267946.svg"
+  },
+  {
+    "revision": "bebc7633696ebc537ae84687da51034d",
+    "url": "/7-wonders-calc/static/media/save.bebc7633.svg"
+  },
+  {
+    "revision": "04116a1af279ddeb8070aee6d563eea9",
+    "url": "/7-wonders-calc/static/media/science.04116a1a.svg"
+  },
+  {
+    "revision": "1502f35a491ddd8b146af6cf7d7f1058",
+    "url": "/7-wonders-calc/static/media/wonders.1502f35a.svg"
+  }
+]);
