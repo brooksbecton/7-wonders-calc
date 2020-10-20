e310073f0059582db7b12e3b4e200f209839b41c
Brooks Becton <bbecton@babelstreet.com>
2020-10-20 19:41:56 -0400

    Init Server Commit

--- web/src/PointsReducer/types.ts
+++
@@ -1,8 +0,0 @@
-export interface IPointType {
-  key: string;
-  value: number;
-  svg: string;
-  color: string;
-}
-
-export type IActionTypes = 'UPDATE_POINT' | 'SAVE_POINTS';
--- web/src/views/dashboard/index.ts
+++
@@ -1,1 +0,0 @@
-export { Calculate } from './components/Calculate';
