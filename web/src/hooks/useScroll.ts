e7dca0184b39b9b81e94a397ddc3c3b890ee99a2
Brooks Becton <bbecton@babelstreet.com>
2020-11-08 09:06:26 -0500

    Raise useScroll Up

--- web/src/hooks/useScroll.ts
+++ web/src/hooks/useScroll.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 interface IScrollPositions {
   x: number;
@@ -11,16 +11,16 @@ export function useScroll() {
     y: window.scrollY,
   };
   const [scrollPositions, setScrollPositions] = useState(
-    defaultScrollPositions,
+    defaultScrollPositions
   );
   const handleScrollChange = () => {
     setScrollPositions({ x: window.scrollX, y: window.scrollY });
   };
   useEffect(() => {
-    window.addEventListener('scroll', handleScrollChange);
+    window.addEventListener("scroll", handleScrollChange);
 
     return () => {
-      window.removeEventListener('scroll', handleScrollChange);
+      window.removeEventListener("scroll", handleScrollChange);
     };
   });
   return scrollPositions;
--- web/src/hooks/useScroll/index.ts
+++
@@ -1,1 +0,0 @@
-export { useScroll } from './useScroll';
