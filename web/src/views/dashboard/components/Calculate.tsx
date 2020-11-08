50c9dba2f853efc1f78ec9bb8e44e333a41e35ef
Brooks Becton <bbecton@babelstreet.com>
2020-11-08 10:59:23 -0500

    Fix focus loss on score keyboard input

--- web/src/views/dashboard/components/Calculate.tsx
+++ web/src/views/dashboard/components/Calculate.tsx
@@ -1,40 +1,31 @@
 import * as React from "react";
 import posed from "react-pose";
 import { PointsContext } from "../../../PointsReducer/PointsContext";
+import { IPointType } from "../../../PointsReducer/types";
 import { CalculateInput } from "./CalculateInput";
 
 export const Calculate: React.FunctionComponent = () => {
   const { pointTypes, setPoints } = React.useContext(PointsContext);
-  const ListContainer = posed.div({
-    enter: { staggerChildren: 50 },
-    exit: { staggerChildren: 20, staggerDirection: -1 },
-  });
 
-  const Item = posed.div({
-    enter: { y: 0, opacity: 1 },
-    exit: { y: 50, opacity: 0 },
-  });
   return (
-    <ListContainer>
-      {pointTypes.map(({ key, value, svg, color }) => (
-        <Item key={key}>
-          <CalculateInput
-            color={color}
-            key={key}
-            label={key}
-            svg={svg}
-            onChange={(newValue) =>
-              setPoints({
-                key,
-                svg,
-                color,
-                value: newValue,
-              })
-            }
-            value={value}
-          />
-        </Item>
+    <div>
+      {pointTypes.map(({ color, key, svg, value }) => (
+        <CalculateInput
+          color={color}
+          key={key}
+          label={key}
+          svg={svg}
+          onChange={(newValue) =>
+            setPoints({
+              key: key,
+              svg: svg,
+              color: color,
+              value: newValue,
+            })
+          }
+          value={value}
+        />
       ))}
-    </ListContainer>
+    </div>
   );
 };
--- web/src/views/dashboard/components/CalculateInput.tsx
+++ web/src/views/dashboard/components/CalculateInput.tsx
@@ -1,22 +1,6 @@
 import { Link } from "react-router-dom";
 import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import { useSpring, animated } from "react-spring";
-
-// Hook
-function usePrevious<T>(value: T) {
-  // The ref object is a generic container whose current property is mutable ...
-  // ... and can hold any value, similar to an instance property on a class
-  const ref = useRef(value);
-
-  // Store current value in ref
-  useEffect(() => {
-    ref.current = value;
-  }, [value]); // Only re-run if value changes
-
-  // Return previous value (happens before update in useEffect above)
-  return ref.current;
-}
 
 interface IProps {
   label: string;
