90840166a661cfb3bba8f4e02020e2dc6d22a76d
Brooks Becton <brooks.becton@bounteous.com>
2023-01-07 21:09:39 -0500

    Add tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -35,6 +35,7 @@ export default class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#f2b851" />
+          <script async defer data-website-id="5f6a718c-be77-4956-8a12-0244d5a13c4c" src="https://umami-production-b0c6.up.railway.app/umami.js"></script>
         </Head>
         <body>
           <Main />
